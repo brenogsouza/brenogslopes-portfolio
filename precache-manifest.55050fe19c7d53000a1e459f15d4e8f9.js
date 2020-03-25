f30300063cc6418872201f88b6bb17b439330b73
brenogsouza <breno.souza.developer@gmail.com>
2020-03-24 21:30:12 -0300

    Updates

---
+++ precache-manifest.55050fe19c7d53000a1e459f15d4e8f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fa319817e37b90c7ad96cd71ae6784",
+    "url": "/brenogslopes-portfolio/index.html"
+  },
+  {
+    "revision": "0b0ce88ae8441de770b7",
+    "url": "/brenogslopes-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "662f9638ee9076e235b5",
+    "url": "/brenogslopes-portfolio/static/js/2.af8261c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brenogslopes-portfolio/static/js/2.af8261c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0ce88ae8441de770b7",
+    "url": "/brenogslopes-portfolio/static/js/main.a71d28a1.chunk.js"
+  },
+  {
+    "revision": "696c3fc9cb57d6400478",
+    "url": "/brenogslopes-portfolio/static/js/runtime-main.4fb8570d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brenogslopes-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
